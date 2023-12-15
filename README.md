cc55434721d236038af66641072c10625d3b4758
Ioannis Kyriakos <Kyriakos.giannis@gmail.com>
2023-12-15 21:29:21 +0000

    changes to make as library

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # python_modules
+
 Contains EDA, data processing and ML modules
+
+## Table of Contents
+
+
+## Installation
+
+```bash
+pip install python_modules
+```
+```bash
+pip install https://github.com/KyriakosJiannis/python_modules.git@main--upgrade
+```
+
+
+## License
+completely free and open-source and licensed under the MIT license.
--- python_modules/eda_charts.py
+++ python_modules/eda_charts.py
@@ -7,16 +7,28 @@ import numpy as np
 def dual_line_barplot(data: pd.DataFrame, target_var: str, feature_var: str, bins: int = None,
                       round_bins: int = None):
     """
-    Create a dual chart (bar chart and line chart) for binary classification cases.
-    The line chart represents the percentage of the target variable, and the bars represent a numerical feature.
+    Create a dual chart bar chart and line chart for binary classification cases where coded as [0,1].
+    The line chart represents the percentage of the target variable, and the bars represent features counts.
 
     :param data: Imported DataFrame.
     :param target_var: Target variable.
-    :param feature_var: Numerical feature.
-    :param bins: Number of bins for numerical feature (default: 10).
+    :param feature_var: feature.
+    :param bins: Number of bins (default: 10).
     :param round_bins: Decimals for rounding.
-    :return: Seaborn dual charts.
+    :return: Seaborn dual chart.
     """
+
+    if not all(data[target_var].isin([0, 1])):
+        raise ValueError("Target and feature variables must contain only 0 and 1.")
+
+    if not isinstance(feature_var, str):
+        raise TypeError("Feature variable should be a string.")
+
+    if bins is not None and not isinstance(bins, int):
+        raise TypeError("Bins should be an integer or None.")
+
+    if round_bins is not None and not isinstance(round_bins, int):
+        raise TypeError("Round bins should be an integer or None.")
 
     # Set the bin number, and create the grouped dataframe
     if pd.api.types.is_numeric_dtype(data[feature_var]):
@@ -35,10 +47,9 @@ def dual_line_barplot(data: pd.DataFrame, target_var: str, feature_var: str, bin
         grouped_data = data.groupby(feature_var)[[feature_var, target_var]].agg(
             {feature_var: 'count', target_var: 'mean'})
 
-    # Rename columns for clarity
+    # Rename columns
     grouped_data.columns = [f'{feature_var} Counts', target_var]
-
-    grouped_data.reset_index(inplace=True)  # Reset index
+    grouped_data.reset_index(inplace=True)
 
     # Convert feature variable to string for categorical display
     grouped_data[feature_var] = grouped_data[feature_var].astype(str)
@@ -54,7 +65,7 @@ def dual_line_barplot(data: pd.DataFrame, target_var: str, feature_var: str, bin
         grouped_data[feature_var] = grouped_data[feature_var].apply(
             lambda x: round_the_brackets(x, decimals=round_bins))
 
-    # Multiply the target variable by 100 for percentage display
+    # Multiply by 100 for percentage display
     grouped_data[target_var] *= 100
 
     # Drop rows with missing values
@@ -73,7 +84,7 @@ def dual_line_barplot(data: pd.DataFrame, target_var: str, feature_var: str, bin
     # Adjust tick sizes
     ax1.tick_params(axis='both', labelsize=8)
 
-    # Specify that we want to share the same x-axis
+    # Create a secondary y-axis chart
     ax2 = ax1.twinx()
 
     # Line plot creation
@@ -83,16 +94,112 @@ def dual_line_barplot(data: pd.DataFrame, target_var: str, feature_var: str, bin
     # Adjust tick sizes
     ax2.tick_params(axis='y', labelsize=8)
 
-    # Automatically adjust subplot parameters
-    plt.tight_layout()
-
     # Show the plot
+    plt.tight_layout()
+    plt.show()
+
+
+def dual_line_bar_boxplot(data: pd.DataFrame, target_var: str, feature_var: str, chart="boxplot", bins: int = None):
+    """
+    Create a dual chart,  bar chart, boxplot and line chart for regression cases.
+    The line chart represents mean target variable on classes, and the bars represent the counts,
+    while boxplots target variable on classes.
+
+    :param chart: boxplot/violin/boxenplot
+    :param data: pandas DataFrame containing the data.
+    :param feature_var: The name of the categorical variable.
+    :param target_var: The name of the continuous variable.
+    :param bins: Number of bins (default: 10).
+    :return: Seaborn dual chart.
+    """
+    # Set the bin number, and create the grouped dataframe
+    if pd.api.types.is_numeric_dtype(data[feature_var]):
+        # Determine the number of bins based on the specified method or default to 10
+        num_bins = bins if bins is not None and bins not in ["auto", "fd", "scott", "rice", "sturges", "doane",
+                                                             "sqrt"] else 10
+
+        # Create bins
+        bins_group = pd.cut(data[feature_var], bins=num_bins, right=False)
+
+        # for boxplot only rawdata
+        # todo merge codes
+        bx_temp = data.copy()
+        bx_temp[feature_var] = pd.cut(bx_temp[feature_var], bins=num_bins, right=False)
+        bx_temp[feature_var] = bx_temp[feature_var].astype('category')
+        bx_temp[feature_var] = bx_temp[feature_var].cat.rename_categories(bins_group)
+        # Convert feature variable to string for categorical display
+        bx_temp[feature_var] = bx_temp[feature_var].astype(str)
+        bx_temp[feature_var] = bx_temp[feature_var].str.replace(", ", "-")
+
+        # Calculate counts and mean for each bin to be used in the chart
+        grouped_data = data.groupby(bins_group)[[feature_var, target_var]].agg(
+            {feature_var: 'count', target_var: 'mean'})
+
+    else:
+        # If the feature variable is not numerical, aggregate using the feature variable directly
+        # todo merge codes bx is temp
+        bx_temp = data.copy()
+
+        grouped_data = data.groupby(feature_var)[[feature_var, target_var]].agg(
+            {feature_var: 'count', target_var: 'mean'})
+
+    # Rename columns for clarity
+    grouped_data.columns = [f'{feature_var} Counts', target_var]
+
+    grouped_data.reset_index(inplace=True)  # Reset index
+
+    # Convert feature variable to string for categorical display
+    # todo
+    grouped_data[feature_var] = grouped_data[feature_var].astype(str)
+    grouped_data[feature_var] = grouped_data[feature_var].str.replace(", ", "-")
+
+    # Create dual chart
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    # Bar plot creation
+    ax1.set_title(f'{feature_var} Distribution with Mean {target_var}', fontsize=12, color='black')
+    ax1.set_xlabel(f'{feature_var} Bins', fontsize=10, color='black')
+    ax1.set_ylabel(f'{feature_var} Counts', fontsize=10, color='black')
+
+    sns.barplot(x=feature_var, y=f'{feature_var} Counts', data=grouped_data, color='C0', ax=ax1)  # alpha=0.6
+
+    # Adjust tick sizes
+    ax1.tick_params(axis='both', labelsize=8)
+
+    # Create a secondary y-axis for the mean line plot and boxplots
+    ax2 = ax1.twinx()
+
+    # Create boxplots for each category
+    # todo data for numerical
+    if chart == 'boxplot':
+        sns.boxplot(x=feature_var, y=target_var, data=bx_temp, ax=ax2, color='grey', fliersize=3)
+    elif chart == 'violin':
+        sns.violinplot(x=feature_var, y=target_var, data=bx_temp, ax=ax2, color='grey',
+                       inner_kws=dict(box_width=15, whis_width=2, color=".8"))
+    elif chart == 'boxenplot':
+        sns.boxenplot(x=feature_var, y=target_var, data=bx_temp, ax=ax2, color='grey',
+                      k_depth="trustworthy", trust_alpha=0.01)
+
+    # Line plot for the mean of the continuous variable with color set to C1
+    sns.lineplot(x=feature_var, y=target_var, data=grouped_data, lw=2, sort=False, ax=ax2, color='C1')
+
+    # Set the labels for the secondary y-axis in black
+    ax2.set_ylabel(f'Mean {target_var}', color='black')
+    ax2.tick_params(axis='y', colors='black')
+
+    # Change the color of the labels on the x-axis to black
+    for label in ax1.get_xticklabels():
+        label.set_color('black')
+
+    # Show plot
+    plt.tight_layout()
     plt.show()
 
 
 def sub_hist_boxplot(data: pd.DataFrame, target_var: str, feature_var: str, stat="density", bins: int = None, ):
     """
-    For classification cases, creates a subplot with a density chart and boxplot on target variable classes for a numerical feature.
+    For classification cases, creates a subplot with a density chart and boxplot on target variable classes
+    for a numerical features.
 
     :param data: Pandas DataFrame.
     :param target_var: Target variable.
@@ -148,6 +255,7 @@ def tabular_plot(data: pd.DataFrame, target_var: str, feature_var: str):
     chart.ax.set_ylim(0, 100)
 
     # Show the plot
+    plt.tight_layout()
     plt.show()
 
 
@@ -174,6 +282,7 @@ def distribution_by_group(data: pd.DataFrame, feature_var: str, by: str, stat="d
     plt.xlabel(feature_var, fontsize=12 * 0.8)
     plt.title(f"Distribution of {feature_var} by {by}", fontsize=12)
 
+    plt.tight_layout()
     plt.show()
 
 
@@ -237,45 +346,46 @@ def plot_distributions(data: pd.DataFrame, stat="count", common_norm=True, by=No
 
 def descriptive_dataframe(data: pd.DataFrame) -> pd.DataFrame:
     """
-    Create a comprehensive descriptive DataFrame from a given DataFrame.
-    includes data types, counts, NaN counts, unique values, top values, frequencies,
+    Create a descriptive DataFrame from a given DataFrame.
+    Includes data types, counts, NaN counts, unique values, top values, frequencies,
     statistical measures, quantiles, kurtosis, skew, and range for numerical variables.
 
     :param data: The input DataFrame.
-    :returns: The comprehensive descriptive DataFrame w
+    :returns: The comprehensive descriptive DataFrame.
     """
+
     # Initial data type, count, and null value calculations
-    count_df = pd.DataFrame({
+    summary_df = pd.DataFrame({
         'Dtype': data.dtypes,
-        'Count': data.shape[0],
+        'Count': data.count(),
         'Non-Null Count': data.notnull().sum(),
-        'NaN Count': data.isna().sum()
+        'NaN Count': data.isna().sum(),
+        'Unique': data.apply(lambda x: x.nunique(), result_type='reduce'),
+        'Top': data.apply(lambda x: x.value_counts().idxmax() if x.dtype == 'O' else np.nan),
+        'Freq': data.apply(lambda x: x.value_counts().max() if x.dtype == 'O' else np.nan)
     })
 
-    # Descriptive statistics
-    describe_df = data.describe(include='all').T
-
-    # Round numeric statistics to 3 decimals
-    numeric_stats = ['mean', 'std', 'min', '25%', '50%', '75%', 'max']
-    for col in numeric_stats:
-        if col in describe_df.columns:
-            describe_df[col] = describe_df[col].apply(lambda x: round(x, 2) if pd.notnull(x) else x)
-
-    # Calculating additional statistics for numeric columns
+    # Descriptive statistics for numeric columns
     numeric_cols = data.select_dtypes(include=[np.number]).columns
-    describe_df.loc[numeric_cols, '5%'] = data[numeric_cols].quantile(0.05).round(3)
-    describe_df.loc[numeric_cols, '95%'] = data[numeric_cols].quantile(0.95).round(3)
-    describe_df.loc[numeric_cols, 'Range'] = (data[numeric_cols].max() - data[numeric_cols].min()).round(3)
-    describe_df.loc[numeric_cols, 'Kurtosis'] = data[numeric_cols].kurtosis().round(3)
-    describe_df.loc[numeric_cols, 'Skew'] = data[numeric_cols].skew().round(3)
+    numeric_desc_df = data[numeric_cols].describe().T
+    numeric_desc_df['5%'] = data[numeric_cols].quantile(0.05)
+    numeric_desc_df['95%'] = data[numeric_cols].quantile(0.95)
+    numeric_desc_df['Range'] = data[numeric_cols].max() - data[numeric_cols].min()
+    numeric_desc_df['Kurtosis'] = data[numeric_cols].kurtosis()
+    numeric_desc_df['Skew'] = data[numeric_cols].skew()
 
-    # Merging all DataFrames into one
-    final_df = pd.concat([count_df, describe_df], axis=1)
+    # Combine the descriptive dataframes
+    combined_df = summary_df.join(numeric_desc_df, how='left', rsuffix='_num')
 
-    # Adjusting the order of columns
-    final_columns = ['Dtype', 'Count', 'NaN Count', 'unique', 'top', 'freq',
+    # Select and reorder columns, ensuring that we don't include the extra columns generated from join
+    final_columns = ['Dtype', 'Count', 'NaN Count', 'Unique', 'Top', 'Freq',
                      'mean', 'std', 'min', '5%', '25%', '50%', '75%', '95%', 'max', 'Range', 'Kurtosis', 'Skew']
-    final_df = final_df[final_columns]
+    final_df = combined_df[final_columns]
+
+    # Round
+
+    round_cols = ['mean', 'std', 'min', '5%', '25%', '50%', '75%', '95%', 'max', 'Range', 'Kurtosis', 'Skew']
+    final_df[round_cols] = final_df[round_cols].apply(lambda x: round(x, 3), axis=0)
 
     return final_df
 
