dd83863a427d1677dadfe1b2bdd2c14cf8b1b53f
Ioannis Kyriakos <Kyriakos.giannis@gmail.com>
2023-12-29 16:59:06 +0200

    dependency tests

--- setup.py
+++ setup.py
@@ -20,23 +20,30 @@ setup(
     author_email='ioannis.kyriakos@gmail.com',
     packages=find_packages(),
     install_requires=[
-        'seaborn>=0.13.0', # General requirement
-        'matplotlib>=3.8.0',
-        'pandas>=2.1.1',
-        'numpy>=1.26.2',
-        'requests>=2.31.0',
-        'scipy>=1.11.4',
-        'scikit-learn>=1.3.0'
+        #'seaborn>=0.13.0', # General requirement
+        #'matplotlib>=3.8.0',
+        #'pandas>=2.1.1',
+        #'numpy>=1.26.2',
+        #'requests>=2.31.0',
+        #'scipy>=1.11.4',
+        #'scikit-learn>=1.3.0'
+        'seaborn>=0.12.0'
+        'matplotlib>=3.7.4',
+        'numpy>=1.22.3',
+        'requests>=2.31.0'
+        'pandas>=2.0.3',
+        'scikit-learn>=1.2.2',
+        'scipy>=1.8.0'
     ],
     extras_require={
         'kaggle': [
-            # Specifying exact versions required for Kaggle environment - not tested only DEV
-            'seaborn>=0.12.0,<0.13.1',
-            'matplotlib>=3.7.4,<3.8.1',
-            'numpy>=1.22.3,<1.26.3',
-            'pandas>=2.0.3,<2.1.2',
-            'scikit-learn>=1.2.2,<1.3.1',
-            'scipy>=1.8.0,<1.11.5'
+            # required for Kaggle environment - not tested only DEV
+            'seaborn==0.12.0',
+            'matplotlib==3.7.4',
+            'numpy==1.22.3',
+            'pandas==2.0.3',
+            'scikit-learn==1.2.2',
+            'scipy==1.8.0'
         ]
     },
     classifiers=[
